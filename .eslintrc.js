b6eb6fd870a3896c793bcfc189ca69722796568a
Lam Nguyen <lam.nguyen@helvar.com>
2025-04-23 13:34:45 +0300

    chore: updated dependencies and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,11 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/recommended", "eslint:recommended"],
   parserOptions: {
     parser: "babel-eslint",
+    sourceType: "module",
+    ecmaVersion: 2015 
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
